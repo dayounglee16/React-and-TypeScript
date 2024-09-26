3aa7103b9942bbe935fa6e0943b1e9d1a870e38f
dayounglee <id46827@gamil.com>
2024-09-26 21:49:31 +0900

    index.jsx -> index.tsx 변환

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+); //함수의 반환값이 Null이 아님을 HTMLElement 타입 단언
 root.render(
   <React.StrictMode>
     <App />
